6b7544a6fb3e6badd2e849b7e663d162a0dbd070
Connor Christie <Connor@connors-mbp.gateway.2wire.net>
2014-06-09 15:59:59 -0500

    Initial Commit

---
+++ Flappy Bird TutorialTests/Flappy_Bird_TutorialTests.swift
@@ -0,0 +1,35 @@
+//
+//  Flappy_Bird_TutorialTests.swift
+//  Flappy Bird TutorialTests
+//
+//  Created by Connor Christie on 6/9/14.
+//  Copyright (c) 2014 Connor Christie. All rights reserved.
+//
+
+import XCTest
+
+class Flappy_Bird_TutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
