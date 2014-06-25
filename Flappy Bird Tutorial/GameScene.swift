a2581d8ac50c5d7e07d8d495ac347c0a9e5b37bb
Connor Christie <Connor@connors-mbp.gateway.2wire.net>
2014-06-25 12:30:20 -0500

    Part 2

--- Flappy Bird Tutorial/GameScene.swift
+++ Flappy Bird Tutorial/GameScene.swift
@@ -11,9 +11,18 @@ import SpriteKit
 class GameScene: SKScene, SKPhysicsContactDelegate
 {
     var bird: SKShapeNode = SKShapeNode(circleOfRadius: 15)
+    var overlay: SKSpriteNode = SKSpriteNode()
     
-    var mainPipe: SKSpriteNode = SKSpriteNode()
-    var pipes: SKSpriteNode[] = []
+    var scoreLabel: SKLabelNode = SKLabelNode(fontNamed: "System-Bold")
+    
+    var ground1: SKSpriteNode = SKSpriteNode()
+    var ground2: SKSpriteNode = SKSpriteNode()
+    
+    var background1: SKSpriteNode = SKSpriteNode()
+    var background2: SKSpriteNode = SKSpriteNode()
+    
+    var mainPipe: Pipe = Pipe()
+    var pipes: Pipe[] = []
     
     var space: Float = 90
     var prevNum: Float = 0
@@ -24,12 +33,63 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     var birdCategory: UInt32 = 1
     var pipeCategory: UInt32 = 2
     
+    var score: Int = 0
+    
+    var movingSpeed: Float = 3.3
+    
+    var isMoving: Bool       = false
+    var isGroundMoving: Bool = true
+    
     override func didMoveToView(view: SKView)
     {
-        mainPipe = SKSpriteNode(color: UIColor.blackColor(), size: CGSize(width: view.bounds.size.width / 6, height: 480))
+        mainPipe = Pipe(color: UIColor.blackColor(), size: CGSize(width: view.bounds.size.width / 6, height: 480))
+        
+        overlay = SKSpriteNode(color: UIColor.grayColor(), size: self.view.bounds.size)
+        
+        overlay.alpha = 0.7
+        overlay.zPosition = 11
+        
+        overlay.position.x += overlay.size.width / 2
+        overlay.position.y += overlay.size.height / 2
+        
+        background1 = SKSpriteNode(imageNamed: "Background")
+        background2 = SKSpriteNode(imageNamed: "Background")
+        
+        background1.position.x = view.bounds.size.width * 0.5
+        background2.position.x = view.bounds.size.width * 1.5
+        
+        background1.position.y = view.bounds.size.height * 0.5
+        background2.position.y = view.bounds.size.height * 0.5
+        
+        background1.texture.filteringMode = SKTextureFilteringMode.Nearest
+        background2.texture.filteringMode = SKTextureFilteringMode.Nearest
+        
+        ground1 = SKSpriteNode(imageNamed: "Ground")
+        ground2 = SKSpriteNode(imageNamed: "Ground")
+        
+        ground1.size.width = view.bounds.size.width + 2
+        ground2.size.width = view.bounds.size.width + 2
+        
+        ground1.position.x = view.bounds.size.width * 0.5
+        ground2.position.x = view.bounds.size.width * 1.5
+        
+        ground1.position.y = ground1.size.height * 0.4
+        ground2.position.y = ground2.size.height * 0.4
+        
+        ground1.physicsBody = SKPhysicsBody(rectangleOfSize: ground1.size)
+        ground2.physicsBody = SKPhysicsBody(rectangleOfSize: ground2.size)
+        
+        ground1.physicsBody.dynamic = false
+        ground2.physicsBody.dynamic = false
+        
+        ground1.zPosition = 10
+        ground2.zPosition = 10
+        
+        ground1.texture.filteringMode = SKTextureFilteringMode.Nearest
+        ground2.texture.filteringMode = SKTextureFilteringMode.Nearest
         
         bird.physicsBody = SKPhysicsBody(circleOfRadius: 15)
-        bird.physicsBody.dynamic = true
+        bird.physicsBody.dynamic = false
         
         bird.physicsBody.contactTestBitMask = pipeCategory
         bird.physicsBody.collisionBitMask   = pipeCategory
@@ -38,22 +98,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         bird.lineWidth = 0
         
         bird.fillColor = UIColor.redColor()
-        bird.position  = CGPoint(x: 150, y: view.bounds.height / 2 - 10);
+        bird.position  = CGPoint(x: 150, y: view.bounds.height / 2 - 10)
+        
+        scoreLabel.position.x = 13
+        scoreLabel.position.y = view.bounds.height - 50
+        
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Left
+        
+        scoreLabel.hidden = true
         
         self.physicsWorld.contactDelegate = self;
         self.physicsWorld.gravity = CGVectorMake(0, -5.0)
         
-        self.spawnPipeRow(0)
+        self.addChild(background1)
+        self.addChild(background2)
+        
+        self.addChild(ground1)
+        self.addChild(ground2)
         
         self.addChild(bird)
+        self.addChild(scoreLabel)
     }
     
     func spawnPipeRow(offs: Float)
     {
         let offset = offs - space / 2
         
-        let pipeBot = (mainPipe as SKSpriteNode).copy() as SKSpriteNode
-        let pipeTop = (mainPipe as SKSpriteNode).copy() as SKSpriteNode
+        let pipeBot = mainPipe.copy() as Pipe
+        let pipeTop = mainPipe.copy() as Pipe
+        
+        pipeBot.texture = SKTexture(imageNamed: "BotPipe")
+        pipeTop.texture = SKTexture(imageNamed: "TopPipe")
+        
+        pipeBot.texture.filteringMode = SKTextureFilteringMode.Nearest
+        pipeTop.texture.filteringMode = SKTextureFilteringMode.Nearest
+        
+        pipeBot.isBottom = true
         
         let xx = self.view.bounds.size.width
         
@@ -99,31 +180,135 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
     {
-        var vel: Float = 200
-        
-        if (self.view.bounds.size.height - bird.position.y < 85)
+        if (!bird.physicsBody.dynamic)
         {
-            vel -= 85 - (self.view.bounds.size.height - bird.position.y)
+            //First touch
+            
+            self.spawnPipeRow(0)
+            
+            bird.physicsBody.dynamic = true
+            
+            bird.physicsBody.velocity = CGVectorMake(0, 175)
+            
+            scoreLabel.hidden = false
+            
+            isMoving = true
+        } else if (isMoving)
+        {
+            var vel: Float = 200
+            
+            if (self.view.bounds.size.height - bird.position.y < 85)
+            {
+                vel -= 85 - (self.view.bounds.size.height - bird.position.y)
+            }
+            
+            bird.physicsBody.velocity = CGVectorMake(0, vel)
+        } else
+        {
+            overlay.removeFromParent()
+            
+            for pi in pipes
+            {
+                pi.removeFromParent()
+            }
+            
+            pipes.removeAll(keepCapacity: false)
+            
+            score = 0
+            
+            bird.physicsBody.dynamic = false
+            bird.position = CGPoint(x: 150, y: view.bounds.size.height / 2 - 10)
+            
+            scoreLabel.hidden = true
+            
+            isGroundMoving = true
         }
-        
-        bird.physicsBody.velocity = CGVectorMake(0, vel)
     }
    
     override func update(currentTime: CFTimeInterval)
     {
-        for (var i = 0; i < pipes.count; i++)
+        if (isGroundMoving)
         {
-            let pipe = pipes[i]
+            ground1.position.x -= movingSpeed
+            ground2.position.x -= movingSpeed
             
-            pipe.position.x -= 1
-            
-            if (i == pipes.count - 1)
+            if (ground1.position.x <= -self.view.bounds.size.width / 2)
             {
-                if (pipe.position.x < self.view.bounds.width - pipe.size.width * 2.0)
-                {
-                    self.spawnPipeRow(self.randomOffset())
-                }
+                ground1.position.x = self.view.bounds.size.width * 1.5 - 2
             }
+            
+            if (ground2.position.x <= -self.view.bounds.size.width / 2)
+            {
+                ground2.position.x = self.view.bounds.size.width * 1.5 - 2
+            }
+            
+            background1.position.x -= movingSpeed / 3
+            background2.position.x -= movingSpeed / 3
+            
+            if (background1.position.x <= -self.view.bounds.size.width / 2)
+            {
+                background1.position.x = self.view.bounds.size.width * 1.5 - 2
+            }
+            
+            if (background2.position.x <= -self.view.bounds.size.width / 2)
+            {
+                background2.position.x = self.view.bounds.size.width * 1.5 - 2
+            }
+            
+            if (isMoving)
+            {
+                for (var i = 0; i < pipes.count; i++)
+                {
+                    let pipe = pipes[i]
+                    
+                    if (pipe.position.x + (pipe.size.width / 2) < 0)
+                    {
+                        pipe.removeFromParent()
+                        
+                        continue
+                    }
+                    
+                    if (pipe.position.x + (pipe.size.width / 2) < self.view.bounds.size.width / 2 && pipe.isBottom && !pipe.pointAdded)
+                    {
+                        score++
+                        
+                        pipe.pointAdded = true
+                    }
+                    
+                    pipe.position.x -= movingSpeed
+                    
+                    if (i == pipes.count - 1)
+                    {
+                        if (pipe.position.x < self.view.bounds.width - pipe.size.width * 2.0)
+                        {
+                            self.spawnPipeRow(self.randomOffset())
+                        }
+                    }
+                }
+                
+                scoreLabel.text = "Score: \(score)"
+            }
+        }
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact!)
+    {
+        if (isMoving)
+        {
+            isMoving = false
+            isGroundMoving = false
+            
+            bird.physicsBody.velocity = CGVectorMake(0, 0)
+            
+            for pi in pipes
+            {
+                pi.physicsBody = nil
+            }
+            
+            self.addChild(overlay)
+        } else
+        {
+            bird.physicsBody.velocity = CGVectorMake(0, 0)
         }
     }
     
@@ -161,7 +346,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         
         return rNum
     }
+    
+    class Pipe: SKSpriteNode
+    {
+        var isBottom: Bool = false
+        
+        var pointAdded: Bool = false
+    }
 }
+
+
 
 
 
